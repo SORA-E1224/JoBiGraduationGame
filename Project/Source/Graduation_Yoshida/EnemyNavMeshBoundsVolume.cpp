c7fb92baf0eba38c8764e33210787769f31ef417
SoraE1224 <sora-e1224@nifty.com>
2018-12-13 15:01:21 +0900

    Adjust Enemy

---
+++ Project/Source/Graduation_Yoshida/EnemyNavMeshBoundsVolume.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EnemyNavMeshBoundsVolume.h"
+
+AEnemyNavMeshBoundsVolume::AEnemyNavMeshBoundsVolume()
+{
+	SupportedAgents.bSupportsAgent15 = 1;
+}
+
+
---
+++ Project/Source/Graduation_Yoshida/EnemyNavMeshBoundsVolume.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NavMesh/NavMeshBoundsVolume.h"
+#include "EnemyNavMeshBoundsVolume.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRADUATION_YOSHIDA_API AEnemyNavMeshBoundsVolume : public ANavMeshBoundsVolume
+{
+	GENERATED_BODY()
+	
+private:
+	AEnemyNavMeshBoundsVolume();
+	
+};
